43d8beeee3161b99e2bf9a03070849de73ac2fc1
Francisco Huertas <francisco@fhuertas.com>
2018-01-19 11:02:27 +0100

    Travis integration

---
+++ dist/build_version.sh
@@ -0,0 +1,10 @@
+REPO=$1
+IMAGE_NAME=$2
+VERSION=$3
+SCALA_VERSION=$4
+DOWNLOAD_URL=$5
+sudo docker build \
+  --build-arg DOWNLOAD_URL=${DOWNLOAD_URL} \
+  --build-arg SCALA_VERSION=${SCALA_VERSION} \
+  --build-arg KAFKA_VERSION=${VERSION} \
+  -t ${REPO}/${IMAGE_NAME}:${VERSION} .
