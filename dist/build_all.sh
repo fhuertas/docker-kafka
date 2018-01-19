8ad206051ecc2a983eed4a14eb9cb6c53fa1fd02
Francisco Huertas <francisco@fhuertas.com>
2018-01-19 13:10:21 +0100

    Add release trigger

--- dist/build_all.sh
+++ dist/build_all.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 BASE_DIR=$(dirname "${BASH_SOURCE[0]}")
+
 source ${BASE_DIR}/../.config
+
 for V in $kafka_versions; do
   ${BASE_DIR}/build_version.sh $docker_repo $docker_image $V $scala_version $base_url
 done
